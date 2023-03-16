7cd52c7f719420addc3f104479f81b2ff3fd15b2
Anton Abramov <abramov282@gmail.com>
2023-03-16 15:18:29 +0300

    final version main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 print("Hello word!")
 =======
 
 print("Hello World!")
 >>>>>>> 5fd0f4d (change main.py)
 
+=======
+>>>>>>> parent of 892f8d3 (changed main.py)
 =======
 >>>>>>> parent of 892f8d3 (changed main.py)
